836ebf608291ef0521a800b2a259898c2d7b6011
steven <renjie373270@163.com>
2018-04-15 11:53:02 +0800

    change update orders method to get

--- src/main/java/com/shadowsocks/web/admin/AdminApi.java
+++ src/main/java/com/shadowsocks/web/admin/AdminApi.java
@@ -36,6 +36,6 @@ public interface AdminApi {
         FINISHED
     }
     @ApiOperation(value = "标记为完成状态", tags = "admin")
-    @RequestMapping(path = "/updateOrder", method = RequestMethod.POST, produces = "application/json; charset=UTF-8")
+    @RequestMapping(path = "/updateOrder", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
     ResponseMessageDto updateOrder(String token, String transactionId, OrderStatus orderStatus);
 }
